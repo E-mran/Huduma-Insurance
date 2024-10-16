2e0f5747b6fff6a95461f0a92c19421a3282d881
prymus <prymuspryme@gmail.com>
2024-10-16 16:06:27 +0300

    removed dotenv module

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import emailjs from "emailjs-com";
-import "dotenv/config";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
